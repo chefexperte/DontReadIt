5985e53e28e653168907c45cd7e1af61a0b0cbb6
Chef Experte <aaron.henning@stud.uni-heidelberg.de>
2022-01-26 05:15:21 +0100

    Added lists Added learning rate

--- statics.py
+++ statics.py
@@ -1,1 +1,25 @@
+"""
+resources used:
+"news" articles from
+    thesun.co.uk
+word lists from
+    wordexample.com
+"""
+
 VERSION_CODE = "0.0.1"
+
+LEARNING_RATE = 0.05
+
+NOUNS = []
+VERBS = []
+ADJECTIVES = []
+TO_BE = []
+EXTRAS = []
+
+
+def ALL_WORDS():
+    return NOUNS + VERBS + ADJECTIVES + TO_BE + EXTRAS
+
+
+def TOTAL():
+    return len(NOUNS) + len(VERBS) + len(ADJECTIVES) + len(TO_BE) + len(EXTRAS)
